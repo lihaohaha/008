eedaf8ddce6303323a2355096455fce9330e75ba
lihaohaha <15910470302@163.com>
2018-07-06 19:00:56 +0800

    初始化

--- scripts/test_08.py
+++ scripts/test_08.py
@@ -2,7 +2,9 @@ import allure
 class Test_001:
     @allure.step(title="第一个测试")
     def test_001_1(self):
+        allure.attach("描述:","我是测试步骤001的描述")
         assert 0
     @allure.step(title="第二个测试")
     def test_001_2(self):
+        allure.attach("描述:", "我是测试步骤002的描述")
         assert 1
